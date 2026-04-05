73929327361842401742b760565a9b90fe7b57eb
chrissszmg <camorales@estud.usfq.edu.ec>
2026-04-04 20:24:01 -0500

    dia en posicion 2

---
+++ mostrarDiaPosicion2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+int main(){
+    
+int const dias = 7;
+string semana [7] = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"};
+
+cout << semana[2] << endl;
+
+
+
+    return 0;
+}
