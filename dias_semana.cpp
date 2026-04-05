1ade869cb4076013ed80c98f2bad2c291d86ad7a
chrissszmg <camorales@estud.usfq.edu.ec>
2026-04-04 20:20:43 -0500

    dias de la semana

---
+++ dias_semana.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+int main(){
+
+int const = 7;
+string dias [7] = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"};
+for ( int i = 0; i < 7; i++)
+cout << dias[i] << endl;
+
+
+
+
+
+
+return 0;
+}
